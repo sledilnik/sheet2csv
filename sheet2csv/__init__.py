cd8afcc1a0de945e573eddd4028771525aafbf31
Andraz Vrhovec <andraz@koofr.net>
2020-03-23 09:22:49 +0100

    wip

--- sheet2csv/__init__.py
+++ sheet2csv/__init__.py
@@ -140,7 +140,7 @@ def sheet2csv(id, range, api_key, filename="export.csv"):
             # pad each row with empty values
             row += [""] * (len(cols) - len(row))
 
-            if len(row[0]) == 0:
+            if len(str(row[0])) == 0:
                 break
 
             csvrow = []
